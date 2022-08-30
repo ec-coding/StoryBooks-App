368b5b4246d5aa87512b8def265f049fdce3934b
Edwin Choi <edwin.choi.wdev@gmail.com>
2022-08-29 21:36:51 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-const Story = require('../models/Story')
+const Story = require('../models/story')
 
 // @desc    Login/Landing page
 // @route   GET / 
